bcb765f47ae2cdb2e25c04551cdc4fe591d66426
Sarah COUDERT <scoudert@e2r11p3.42.fr>
2014-12-08 15:58:46 +0100

    commit

---
+++ CORRECTIONS/GNL/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scoudert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/08 11:21:03 by scoudert          #+#    #+#             */
+/*   Updated: 2014/12/08 14:05:27 by scoudert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include "libft/includes/libft.h"
+#include "get_next_line.h"
+#include <stdlib.h>
+
+int		main()
+{
+	char	*s;
+	int i;
+
+	i = 42;
+	int		file = open("ocean1.txt", O_RDONLY);
+	while (i != 0)
+	{
+		s = NULL;
+		i = get_next_line(file, &s);
+		if (i > 0)
+		{
+			ft_putendl(s);
+			free(s);
+		}
+	}
+	close(file);
+	return (i);
+}
