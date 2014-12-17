304bf9e901bbae634aa27ed6e875bd94d2f7b1e4
MitsukiPink <sarahcoudert@gmx.fr>
2014-12-17 19:40:22 +0100

    poll

---
+++ CORRECTIONS/tab_merge_sort/main.c
@@ -0,0 +1,33 @@
+#include "includes/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+int *MergeSort(int *tab, int count);
+
+int	main()
+{
+	int	*tab;
+	int	i;
+	int j;
+
+	j = 0;
+	tab = (int*)malloc(sizeof(int) * 999999);
+	i = 0;
+	srand(time(NULL));
+	while (j <= 999999)
+	{
+		tab[j] = rand();
+		j++;
+	}
+	MergeSort(tab, 999999);
+	while (i < 999999)
+	{
+		ft_putnbr(tab[i]);
+		ft_putchar('\n');
+		if (tab[i] > tab[i+1] && i + i <= 999999)
+		ft_putendl("NOPE");
+		i++;
+	}
+	return (0);
+}
---
+++ ft_ls/libft/ft_lstcountelements.c
@@ -0,0 +1,18 @@
+#
+#
+#
+
+#include "includes/libft.h"
+
+int		ft_lstcountelements(t_list *lst)
+{
+	int		i;
+
+	i = 0;
+	while (lst)
+	{
+		i++;
+		lst = lst->next;
+	}
+	return (i);
+}
